6f57e5e60b9906c2b31d933fda9f62e4a7f150e5
Thomas Edgemon <44883486+thomasedgemon@users.noreply.github.com>
2024-02-17 20:42:27 -0600

    Add files via upload

---
+++ graph_stuff_3.py
@@ -0,0 +1,8 @@
+#using different storage types for perhaps easier parsing of vertices and their edges
+
+A_edges = ["B", "C", "D", "E"]
+B_edges = ["C", "D", "E"]
+C_edges = ["D", "E"]
+D_edges = ["E"]
+
+Edges = ["AB", "AC", "AD", "AE", "BC", "BD", "BE", "CD", "CE", "DE"]

5e6a4cd8f8137963eb9a639affb9246e33379d38
Thomas Edgemon <44883486+thomasedgemon@users.noreply.github.com>
2024-02-17 21:06:21 -0600

    Delete graph_stuff_3.py

--- graph_stuff_3.py
+++
@@ -1,8 +0,0 @@
-#using different storage types for perhaps easier parsing of vertices and their edges
-
-A_edges = ["B", "C", "D", "E"]
-B_edges = ["C", "D", "E"]
-C_edges = ["D", "E"]
-D_edges = ["E"]
-
-Edges = ["AB", "AC", "AD", "AE", "BC", "BD", "BE", "CD", "CE", "DE"]
